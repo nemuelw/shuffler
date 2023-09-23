59eda681f12e453e57a45c2b5641cd4ee1838e67
Nemuel Wainaina <bR0S3@protonmail.com>
2023-09-23 06:01:29 +0300

    Add Fisher-Yates and Knuth algorithm

--- shuffler.go
+++ shuffler.go
@@ -2,8 +2,26 @@ package shuffler
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func FisherYatesShuffle[T any](arr []T) {
-	fmt.Println("Hello friend")
+	rand.Seed(time.Now().UnixNano())
+	n := len(arr)
+	for i := n - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		arr[i], arr[j] = arr[j], arr[i]
+	}
 }
+
+func KnuthShuffle[T any](arr []T) {
+	rand.Seed(time.Now().UnixNano())
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		j := i + rand.Intn(n - i)
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+}
+
+
