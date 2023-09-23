b9029c3ca2684217cc38036cec7771fcd382cc88
Nemuel Wainaina <bR0S3@protonmail.com>
2023-09-23 06:19:05 +0300

    Add the Durstenfeld algorithm

--- shuffler.go
+++ shuffler.go
@@ -32,4 +32,13 @@ func SattolosShuffle[T any](arr []T) {
 	}
 }
 
+func DurstenfeldShuffle[T any](arr []T) {
+	rand.Seed(time.Now().UnixNano())
+	n := len(arr)
+	for i := n - 1; i >= 1; i-- {
+		j := rand.Intn(i + 1)
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+}
+
 
