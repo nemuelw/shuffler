7a36ce3f7eaa788e156b210b2c8cb38b8e63ced3
Nemuel Wainaina <bR0S3@protonmail.com>
2023-09-23 06:30:26 +0300

    Add the Riffle/Perfect shuffle algorithm

--- shuffler.go
+++ shuffler.go
@@ -49,3 +49,29 @@ func CohensShuffle[T any](arr []T) {
 		arr[i], arr[j] = arr[j], arr[i]
 	}
 }
+
+func RiffleShuffle[T any](arr []T) {
+	rand.Seed(time.Now().UnixNano())
+	n := len(arr)
+	middle := n / 2
+	left_half := arr[:middle]
+	right_half := arr[middle:]
+	shuffled := make([]T, 0, n)
+
+	for len(left_half) > 0 && len(right_half) > 0 {
+		// randomly select either left or right
+		if rand.Float64() < 0.5 {
+			shuffled = append(shuffled, left_half[0])
+			left_half = left_half[1:]
+		} else {
+			shuffled = append(shuffled, right_half[0])
+			right_half = right_half[1:]
+		}
+	}
+
+	// append any remaining cards
+	shuffled = append(shuffled, left_half...)
+	shuffled = append(shuffled, right_half...)
+
+	copy(arr, shuffled)
+}
